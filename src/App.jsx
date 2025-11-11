eb80d5a00c58bf59e0bfe0e456a53415795bb348
Prashant7744 <peashant7744@gmail.com>
2025-11-11 19:48:14 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>Welcome to RASNET 🚀</h1>
+      <p>We are building this together.</p>
+    </div>
+  );
+}
+
+export default App;
